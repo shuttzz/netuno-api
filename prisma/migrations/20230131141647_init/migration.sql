8cdf92f7383ca89d10d7cc15acc8a93e5d11108a
shuttzz <netomenezesucg@gmail.com>
2023-01-31 11:26:17 -0300

    :sparkles: Criada feature de excluir uma categoria

---
+++ prisma/migrations/20230131141647_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" VARCHAR(36) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(150) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "avatar_url" TEXT,
+    "avatar_key" VARCHAR(255),
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "firebase_token" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "passwords_recovery" (
+    "id" VARCHAR(36) NOT NULL,
+    "user_id" VARCHAR(36) NOT NULL,
+    "code" VARCHAR(36) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expires_in" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "passwords_recovery_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" VARCHAR(36) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "user_id" VARCHAR(36) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "passwords_recovery_code_key" ON "passwords_recovery"("code");
+
+-- AddForeignKey
+ALTER TABLE "passwords_recovery" ADD CONSTRAINT "passwords_recovery_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
